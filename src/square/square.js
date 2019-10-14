983469df1c8d776a04b94371294e8b7bd55cbc96
chenpengxu <chenpengxu@dding.cn>
2019-10-14 18:29:07 +0800

    feat: 添加点击事件，再放个里面绘制‘X’

--- src/square/square.js
+++ src/square/square.js
@@ -1,11 +1,19 @@
 import React from 'react';
 class Square extends React.Component {
+  constructor (p) {
+    super(p)
+    this.state = {
+      value: null
+    }
+
+  }
   render() {
     return (
       <button className="square" onClick={() => {
-        alert('click')
+        // alert('click')
+        this.setState({ value: 'X' }) // 每次在组件中调用 setState 时，React 都会自动更新其子组件
       }}>
-        {this.props.value}
+        {this.state.value || '-'}
       </button>
     );
   }
